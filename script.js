8b91165f41bda3bf554598764ae8406417d9f724
Jason McDonald <jasonmcd65@gmail.com>
2020-10-29 14:31:22 -0500

    added generate lower and capital

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ var generateBtn = document.querySelector("#generate");
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
     "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // There are more special characters to add
-var specialChars = ['@', '#', '$', '%', '<', '^', '<'];
+
+var specialChars = ['!', '"', '#', '$', '%', '&', '\'', '(', ')',
+    '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?',
+    '@', '[', '\\', ']', '^', '_', '`', '{', '}', '|', '~'];
+
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 //codes
@@ -15,7 +18,7 @@ const specialsKey = 3;
 
 //used as an upper limit to generate a random number
 //random number is used to select one of the above arrays
-const arrayChoiceLimit = 4; 
+const arrayChoiceLimit = 4;
 
 // Write password to the #password input
 function writePassword() {
@@ -29,7 +32,7 @@ function writePassword() {
 //generates password
 function generatePassword() {
     var generatedString = "";
-    var addNums = false;
+    var useNums = false;
     var useLower = false;
     var useCaps = false;
     var useSpecial = false;
@@ -38,12 +41,12 @@ function generatePassword() {
 
     //get criteria
     while (flag) {
-        addNums = confirm("Would you like to use numbers? (Click cancel for no)");
+        useNums = confirm("Would you like to use numbers? (Click cancel for no)");
         useLower = confirm("Would you like to use lower case letters? (Click cancel for no)");
         useCaps = confirm("Would you like to use capital letters? (Click cancel for no)");
         useSpecial = confirm("Would you like to use special characters? (Click cancel for no)");
 
-        if(addNums === false && useLower === false && useCaps === false && useSpecial === false){
+        if (useNums === false && useLower === false && useCaps === false && useSpecial === false) {
             alert("Error: Please say yes to at least one option");
         } else {
             flag = false;
@@ -53,34 +56,45 @@ function generatePassword() {
     flag = true;
     var valid = false;
 
-    while(flag){
+    while (flag) {
         //input should be a whole number where 8 <= input <= 128
         input = prompt("How long do you want your password to be? (between 8 and 128 characters)");
 
         valid = validateInput(input);
-        if(valid === false){
+        if (valid === false) {
             alert("Error: Please enter a number between 8 and 128");
         } else {
             flag = false;
             length = parseInt(input);
         }
     }
-   
-    //now generate the password based on criteria
-    while(generatedString.length < length){   
 
+    //now generate the password based on criteria
+    while (generatedString.length < length) {
+        var newChar = '';
         var randNumber = rand(arrayChoiceLimit);
-        console.log(randNumber);
-        if(randNumber === numsKey){
+
+        if (randNumber === numsKey && useNums) {
             //generate a random number for string
-            var newChar = generateNumber();
+            console.log("number");
+            newChar = generateNumber();
             generatedString = generatedString + newChar;
-        }else if(randNumber === lowersKey){
+
+        } else if (randNumber === lowersKey && useLower) {
             //generate a random lowercase letter
-        }else if(randNumber === capsKey){
+            console.log("lower");
+            newChar = generateLowerLetter();
+            generatedString = generatedString + newChar;
+        } else if (randNumber === capsKey && useCaps) {
             //generate a random capital letter
-        }else if(randNumber === specialsKey){
+            console.log("capital");
+            newChar = generateCapitalLetter();
+            generatedString = generatedString + newChar;
+        } else if (randNumber === specialsKey && useSpecial) {
+            console.log("special");
             //generate a random special character
+            newChar = generateSpecial();
+            generatedString = generatedString + newChar;
         }
 
     }
@@ -92,29 +106,41 @@ function generatePassword() {
 
 
 // validateInput(input) takes a variable and checks for an int and is bewteen two numbers
-function validateInput(input){
+function validateInput(input) {
 
     //make sure input is number first
     var num = parseInt(input);
 
-    if(Number.isInteger(num) === false ){
+    if (Number.isInteger(num) === false) {
         return false;
     }
 
-    if(input < 8 || input > 128){
+    if (input < 8 || input > 128) {
         return false;
     }
 
     return true;
 }
 
-function generateNumber(){
+function generateNumber() {
     var newNum = numbers[rand(numbers.length)];
     return newNum;
 }
 
+function generateLowerLetter() {
+    var newLetter = alphabet[rand(alphabet.length)];
+    return newLetter;
+}
+
+function generateCapitalLetter() {
+    var newLetter = alphabet[rand(alphabet.length)];
+    return newLetter.toUpperCase();
+}
+
+
+
 //helper function rand(); makes it easier to generate a number
-function rand(upperLimit){
+function rand(upperLimit) {
     return Math.floor(Math.random() * upperLimit)
 }
 
